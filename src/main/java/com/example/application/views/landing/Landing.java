5482f5bad5b2bc123662c3e64b4bb9a868d7b687
santhitak <santhita.sk@gmail.com>
2022-08-29 02:54:43 +0700

    init

--- src/main/java/com/example/application/views/landing/Landing.java
+++ src/main/java/com/example/application/views/landing/Landing.java
@@ -1,29 +1,26 @@
-package com.example.application.views.list;
+package com.example.application.views.landing;
 
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @PageTitle("list")
-@Route(value = "list")
-public class ListView extends VerticalLayout {
+@Route(value = "")
+public class Landing extends VerticalLayout {
 
-    public ListView() {
+    public Landing() {
         setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
 
         add(new H2("This place intentionally left empty"));
         add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
 
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
         getStyle().set("text-align", "center");
     }
 
