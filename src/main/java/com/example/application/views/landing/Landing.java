3cef17551d5916017c7ed508c3380787c502ec1f
santhitak <santhita.sk@gmail.com>
2022-08-29 04:18:45 +0700

    feat: unfinished menubar

--- src/main/java/com/example/application/views/landing/Landing.java
+++ src/main/java/com/example/application/views/landing/Landing.java
@@ -1,22 +1,29 @@
 package com.example.application.views.landing;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@PageTitle("list")
+@PageTitle("Uber Eats | Food delivery and Takeout")
 @Route(value = "")
 public class Landing extends VerticalLayout {
 
     public Landing() {
-        setSpacing(false);
+        Button btnLogin = new Button("Log in");
+        Button btnSignup = new Button("Sign up");
+        FormLayout menu = new FormLayout();
 
-        add(new H2("UberEats"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        menu.setResponsiveSteps(new FormLayout.ResponsiveStep("200px", 3));
+        menu.setSizeFull();
+        menu.add(new H2("UberEats"), btnLogin, btnSignup);
+
+
+        add(menu);
 
         setSizeFull();
         setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
