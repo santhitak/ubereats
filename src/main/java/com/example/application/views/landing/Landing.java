22abd159cdad8d6c5b3029f3def267f2721c0696
santhitak <santhita.sk@gmail.com>
2022-08-30 01:06:37 +0700

    feat: knocked components

--- src/main/java/com/example/application/views/landing/Landing.java
+++ src/main/java/com/example/application/views/landing/Landing.java
@@ -1,12 +1,13 @@
 package com.example.application.views.landing;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.BoxSizing;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -18,9 +19,9 @@ public class Landing extends VerticalLayout{
 
     public Landing() {
         Button btnLogin = new Button("Log in", USER.create());
-        btnLogin.setClassName("btnLogin btn");
+        btnLogin.setClassName("btnBlackText btn");
         Button btnSignup = new Button("Sign up");
-        btnSignup.setClassName("btnSignup btn");
+        btnSignup.setClassName("btnWhiteText btn");
 
         HorizontalLayout navbar = new HorizontalLayout();
 
@@ -45,13 +46,41 @@ public class Landing extends VerticalLayout{
         navbar.setSizeFull();
         navbar.setAlignItems(Alignment.BASELINE);
         navbar.setJustifyContentMode(JustifyContentMode.BETWEEN);
+        navbar.setHeight("auto");
 
-        add(navbar);
+        VerticalLayout orderFood = new VerticalLayout();
+
+        H1 your_door = new H1("Order food to your door");
+        your_door.setClassName("text-xl text");
+
+        FormLayout findFood = new FormLayout();
+        TextField address = new TextField();
+        address.setPlaceholder("Enter delivery address");
+
+        Select<String> time = new Select<>();
+        time.setItems("Delivery now", "Delivery later", "Self Pick-up");
+        time.setValue("Delivery now");
+
+        Button find = new Button("Find Food");
+        find.setClassName("btnWhiteText");
+
+        findFood.setResponsiveSteps(
+                new FormLayout.ResponsiveStep("0", 1),
+                new FormLayout.ResponsiveStep("320px", 3)
+        );
+
+        findFood.add(address, time, find);
+        orderFood.add(your_door, findFood);
+        orderFood.setHeight("60vh");
+        orderFood.setWidth("45vw");
+        orderFood.setJustifyContentMode(JustifyContentMode.CENTER);
+
+        add(navbar, orderFood);
 
         setSizeFull();
         setClassName("setBackgroundImage");
-        setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.START);
+        setDefaultHorizontalComponentAlignment(Alignment.START);
     }
 
 }
