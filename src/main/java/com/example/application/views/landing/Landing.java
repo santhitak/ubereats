4b17a50c7040c079340736241655f1a4849dde4d
santhitak <santhita.sk@gmail.com>
2022-08-30 00:22:04 +0700

    feat: full navbar

--- src/main/java/com/example/application/views/landing/Landing.java
+++ src/main/java/com/example/application/views/landing/Landing.java
@@ -2,6 +2,8 @@ package com.example.application.views.landing;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.BoxSizing;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -16,14 +18,30 @@ public class Landing extends VerticalLayout{
 
     public Landing() {
         Button btnLogin = new Button("Log in", USER.create());
-        btnLogin.setClassName("btnLogin");
+        btnLogin.setClassName("btnLogin btn");
         Button btnSignup = new Button("Sign up");
+        btnSignup.setClassName("btnSignup btn");
+
         HorizontalLayout navbar = new HorizontalLayout();
+
         HorizontalLayout btn = new HorizontalLayout();
         btn.add(btnLogin, btnSignup);
         btn.setSpacing(true);
 
-        navbar.add(new H2("UberEats"), btn);
+        H2 uber = new H2("Uber");
+        H2 eats = new H2("Eats");
+        uber.setClassName("text uber");
+        eats.setClassName("text eats");
+        HorizontalLayout uber_eats = new HorizontalLayout();
+        uber_eats.setSpacing(false);
+        uber_eats.add(uber, eats);
+
+        HorizontalLayout rightItem = new HorizontalLayout();
+        rightItem.add(MENU.create(), uber_eats);
+        rightItem.setClassName("text");
+        rightItem.setAlignItems(Alignment.BASELINE);
+
+        navbar.add(rightItem, btn);
         navbar.setSizeFull();
         navbar.setAlignItems(Alignment.BASELINE);
         navbar.setJustifyContentMode(JustifyContentMode.BETWEEN);
@@ -35,4 +53,5 @@ public class Landing extends VerticalLayout{
         setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
     }
+
 }
